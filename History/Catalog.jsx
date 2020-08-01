129bd3b94ae0e4f61a10ce592f6c061a94a57958
hunzsig <mzyhaohaoren@qq.com>
2020-08-01 11:57:53 +0800

    feat(Catalog): *

---
+++ History/Catalog.jsx
@@ -0,0 +1,116 @@
+import './Catalog.less';
+import React, {Component} from 'react';
+import {Menu} from 'antd';
+import {BulbFilled, VerticalAlignTopOutlined} from '@ant-design/icons';
+import {Auth, History, LocalStorage} from 'h-react-antd';
+
+class Catalog extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      catalog: {
+        theme: LocalStorage.get('h-react-catalog-theme') || 'light',
+        collapsed: LocalStorage.get('h-react-catalog-collapsed') || false,
+      },
+    }
+  }
+
+
+  renderSub = (router) => {
+    router = router || this.children;
+    return (
+      router.map((val) => {
+        if (val.hide === true) {
+          return null;
+        } else if (this.state.path.includes(val.jumpPath)) {
+          switch (this.theme.pathHideType) {
+            case 'hidden':
+              return null;
+            case 'disabled':
+            default:
+              return (
+                <Menu.Item
+                  key={val.jumpPath}
+                  disabled
+                >
+                  {val.icon !== undefined ? val.icon : ''}<span>{val.name}</span>
+                </Menu.Item>
+              );
+          }
+        } else if (val.children !== undefined && val.children.length > 0) {
+          return (
+            <Menu.SubMenu
+              key={val.jumpPath}
+              disabled={val.disabled}
+              title={<span>{val.icon !== undefined ? val.icon : ''}<span>{val.name}</span></span>}
+            >
+              {this.renderSub(val.children)}
+            </Menu.SubMenu>
+          );
+        }
+        return (
+          <Menu.Item
+            key={val.jumpPath}
+            disabled={val.disabled}
+          >
+            {val.icon !== undefined ? val.icon : ''}<span>{val.name}</span>
+          </Menu.Item>
+        );
+      })
+    );
+  };
+
+  render() {
+    return (
+      <div className={`catalog ${this.state.catalog.theme}`}>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme={this.state.catalog.theme}
+          inlineCollapsed={this.state.catalog.collapsed}
+        >
+          <Menu.Item key="1" icon={<VerticalAlignTopOutlined/>}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VerticalAlignTopOutlined/>}>
+            Option 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<VerticalAlignTopOutlined/>}>
+            Option 3
+          </Menu.Item>
+          <Menu.SubMenu key="sub1" icon={<VerticalAlignTopOutlined/>} title="Navigation One">
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </Menu.SubMenu>
+          <Menu.SubMenu key="sub2" icon={<VerticalAlignTopOutlined/>} title="Navigation Two">
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <Menu.SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </Menu.SubMenu>
+          </Menu.SubMenu>
+        </Menu>
+        <span className="catalog-switch">
+              <BulbFilled onClick={() => {
+                this.state.catalog.theme = this.state.catalog.theme === 'dark' ? 'light' : 'dark';
+                LocalStorage.set('h-react-catalog-theme', this.state.catalog.theme);
+                this.setState({catalog: this.state.catalog});
+              }}/>
+              <VerticalAlignTopOutlined rotate={this.state.catalog.collapsed ? 90 : -90} onClick={() => {
+                this.state.catalog.collapsed = !this.state.catalog.collapsed || false;
+                LocalStorage.set('h-react-catalog-collapsed', this.state.catalog.collapsed);
+                this.setState({catalog: this.state.catalog});
+              }}/>
+            </span>
+      </div>
+    );
+  }
+}
+
+export default Catalog;
--- History/Initial.jsx
+++ History/Initial.jsx
@@ -1,11 +1,11 @@
 import './Initial.less';
 import React, {Component} from 'react';
 import Loadable from 'react-loadable';
-import {ConfigProvider, Menu} from "antd";
+import {ConfigProvider} from "antd";
 import {Auth, Parse, History, I18nConfig, LocalStorage} from "h-react-antd";
-import {VerticalAlignTopOutlined, BulbFilled} from '@ant-design/icons';
 import Loading from "./Loading";
 import Login from "./Login";
+import Catalog from "./Catalog";
 
 
 class Initial extends Component {
@@ -17,10 +17,6 @@ class Initial extends Component {
     this.state = {
       ...props.data,
       logging: Auth.isLogging(),
-      catalog: {
-        theme: LocalStorage.get('h-react-catalog-theme') || 'light',
-        collapsed: LocalStorage.get('h-react-catalog-collapsed') || false,
-      },
       subPages: [
         {url: location.pathname === '/' ? location.url : '/', ...History.router['/']},
       ],
@@ -45,51 +41,7 @@ class Initial extends Component {
     if (this.state.logging) {
       return (
         <div className="container">
-          <div className={`catalog ${this.state.catalog.theme}`}>
-            <Menu
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              mode="inline"
-              theme={this.state.catalog.theme}
-              inlineCollapsed={this.state.catalog.collapsed}
-            >
-              <Menu.Item key="1" icon={<VerticalAlignTopOutlined/>}>
-                Option 1
-              </Menu.Item>
-              <Menu.Item key="2" icon={<VerticalAlignTopOutlined/>}>
-                Option 2
-              </Menu.Item>
-              <Menu.Item key="3" icon={<VerticalAlignTopOutlined/>}>
-                Option 3
-              </Menu.Item>
-              <Menu.SubMenu key="sub1" icon={<VerticalAlignTopOutlined/>} title="Navigation One">
-                <Menu.Item key="5">Option 5</Menu.Item>
-                <Menu.Item key="6">Option 6</Menu.Item>
-                <Menu.Item key="7">Option 7</Menu.Item>
-                <Menu.Item key="8">Option 8</Menu.Item>
-              </Menu.SubMenu>
-              <Menu.SubMenu key="sub2" icon={<VerticalAlignTopOutlined/>} title="Navigation Two">
-                <Menu.Item key="9">Option 9</Menu.Item>
-                <Menu.Item key="10">Option 10</Menu.Item>
-                <Menu.SubMenu key="sub3" title="Submenu">
-                  <Menu.Item key="11">Option 11</Menu.Item>
-                  <Menu.Item key="12">Option 12</Menu.Item>
-                </Menu.SubMenu>
-              </Menu.SubMenu>
-            </Menu>
-            <span className="catalog-switch">
-              <BulbFilled onClick={() => {
-                this.state.catalog.theme = this.state.catalog.theme === 'dark' ? 'light' : 'dark';
-                LocalStorage.set('h-react-catalog-theme', this.state.catalog.theme);
-                this.setState({catalog: this.state.catalog});
-              }}/>
-              <VerticalAlignTopOutlined rotate={this.state.catalog.collapsed ? 90 : -90} onClick={() => {
-                this.state.catalog.collapsed = !this.state.catalog.collapsed || false;
-                LocalStorage.set('h-react-catalog-collapsed', this.state.catalog.collapsed);
-                this.setState({catalog: this.state.catalog});
-              }}/>
-            </span>
-          </div>
+          <Catalog/>
           <div className="exhibition">
             <div className="tabs">
 
